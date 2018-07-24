1395832495e80324f02f97d6ad7822628824f4e5
Jubar Ramsey <jramsey08@gmail.com>
2018-07-24 18:26:39 -0400

    Add files via upload

--- forms/admin/menu.php
+++ forms/admin/menu.php
@@ -33,8 +33,8 @@ if($Login == "1"){
             }else{
                 $query = "SELECT * FROM admin WHERE url ='$Article_Url' AND active='1' AND trash='0' AND id!='$Article_Id' AND webid='$WebId'";
             }
-            $result = mysql_query($query) or die(mysql_error());
-            $row = mysql_fetch_array($result);
+            $result = mysqli_query($CwDb, $query);
+            $row = mysqli_fetch_assoc($result);
             if($row['id'] != ""){
                 $Article_Url = $Article_Name . $Rand;
             }else{
@@ -65,40 +65,30 @@ if($Login == "1"){
     if($Article_Id == ""){
         $Manual_Message = "Created admin record";
 
-        mysql_query("INSERT INTO admin(type, content, active, name, list, category, other, access, url, theme, webid) VALUES('$Article_Type', '$Article_Content',  '$Article_Active', '$Article_Name', 
-        '$Article_List', '$Article_Category','$Article_Other', '$Article_Access', '$Article_Url', '$Article_Theme', '$WebId') ")or die(mysql_error());
+        mysqli_query($CwDb,"INSERT INTO admin(type, content, active, name, list, category, other, access, url, theme, webid) VALUES('$Article_Type', '$Article_Content',  '$Article_Active', '$Article_Name', 
+        '$Article_List', '$Article_Category','$Article_Other', '$Article_Access', '$Article_Url', '$Article_Theme', '$WebId') ");
 
 
     }else{
         
     $query = "SELECT * FROM admin WHERE id='$Article_Id'";
-	$result = mysql_query($query) or die(mysql_error());
-	$Article = mysql_fetch_array($result);
+    $result = mysqli_query($CwDb, $query);
+    $Article = mysqli_fetch_assoc($result);
     $Article = CwOrganize($Article,$Array);
     $Article = Cw_Filter_Array($Article);
 
 
 // UPDATE THE DATABASE WITH ANY NEW/OLD INFORMATION \\
-    $result = mysql_query("UPDATE admin SET type='$Article_Type' WHERE id='$Article_Id'") 
-	or die(mysql_error()); 
-	$result = mysql_query("UPDATE admin SET content='$Article_Content' WHERE id='$Article_Id' ") 
-	or die(mysql_error()); 
-	$result = mysql_query("UPDATE admin SET active='$Article_Active' WHERE id='$Article_Id' ") 
-	or die(mysql_error()); 
-	$result = mysql_query("UPDATE admin SET name='$Article_Name' WHERE id='$Article_Id' ") 
-	or die(mysql_error()); 
-	$result = mysql_query("UPDATE admin SET list='$Article_List' WHERE id='$Article_Id' ") 
-	or die(mysql_error());
-	$result = mysql_query("UPDATE admin SET category='$Article_Category' WHERE id='$Article_Id' ") 
-	or die(mysql_error());
-	$result = mysql_query("UPDATE admin SET other='$Article_Other' WHERE id='$Article_Id' ") 
-	or die(mysql_error());
-	$result = mysql_query("UPDATE admin SET access='$Article_Access' WHERE id='$Article_Id' ") 
-	or die(mysql_error());
-	$result = mysql_query("UPDATE admin SET url='$Article_Url' WHERE id='$Article_Id'") 
-	or die(mysql_error());
-	$result = mysql_query("UPDATE admin SET theme='$Article_Theme' WHERE id='$Article_Id' ") 
-	or die(mysql_error());
+    $result = mysqli_query($CwDb,"UPDATE admin SET type='$Article_Type' WHERE id='$Article_Id'"); 
+	$result = mysqli_query($CwDb,"UPDATE admin SET content='$Article_Content' WHERE id='$Article_Id' "); 
+	$result = mysqli_query($CwDb,"UPDATE admin SET active='$Article_Active' WHERE id='$Article_Id' "); 
+	$result = mysqli_query($CwDb,"UPDATE admin SET name='$Article_Name' WHERE id='$Article_Id' "); 
+	$result = mysqli_query($CwDb,"UPDATE admin SET list='$Article_List' WHERE id='$Article_Id' ");
+	$result = mysqli_query($CwDb,"UPDATE admin SET category='$Article_Category' WHERE id='$Article_Id' ");
+	$result = mysqli_query($CwDb,"UPDATE admin SET other='$Article_Other' WHERE id='$Article_Id' ");
+	$result = mysqli_query($CwDb,"UPDATE admin SET access='$Article_Access' WHERE id='$Article_Id' ");
+	$result = mysqli_query($CwDb,"UPDATE admin SET url='$Article_Url' WHERE id='$Article_Id'") ;
+	$result = mysqli_query($CwDb,"UPDATE admin SET theme='$Article_Theme' WHERE id='$Article_Id' ");
     }
 
 // TRACKS CHANGES MADE FROM USERS \\
